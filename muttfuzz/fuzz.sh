9327fffe4014bc2cd2da7bcfaed5ffe5ac71ec92
dergoegge <n.goeggi@gmail.com>
2024-09-11 16:38:59 +0100

    muttfuzz: account for timeouts correctly

--- muttfuzz/fuzz.sh
+++ muttfuzz/fuzz.sh
@@ -5,9 +5,10 @@ set -xe
 mkdir --parents $REACHABILITY_CORPUS
 touch $EVAL_RESULTS
 
-muttfuzz "./bitcoin/build/src/test/fuzz/fuzz" ./bitcoin/build/src/test/fuzz/fuzz \
+muttfuzz "./bitcoin/build/src/test/fuzz/fuzz -timeout=$LIBFUZZER_TIMEOUT" ./bitcoin/build/src/test/fuzz/fuzz \
   --time_per_mutant $TIME_PER_MUTANT \
   --reachability_check_cmd "./bitcoin/build/src/test/fuzz/fuzz -runs=1 $REACHABILITY_CORPUS" \
   --score --avoid_repeats --verbose \
   --budget $EVAL_BUDGET \
+  --no_timeout_kills \
   --save_results $EVAL_RESULTS $@
