09b04765b3eefaab2925d3d93cff66aa0815c685
dergoegge <n.goeggi@gmail.com>
2024-09-11 16:52:18 +0100

    muttfuzz: Don't start from empty corpus by default

--- muttfuzz/fuzz.sh
+++ muttfuzz/fuzz.sh
@@ -4,9 +4,12 @@ set -xe
 
 mkdir --parents $REACHABILITY_CORPUS
 touch $EVAL_RESULTS
+mkdir --parents $LIBFUZZER_WORK_CORPUS
 
-muttfuzz "./bitcoin/build/src/test/fuzz/fuzz -timeout=$LIBFUZZER_TIMEOUT" ./bitcoin/build/src/test/fuzz/fuzz \
+muttfuzz "./bitcoin/build/src/test/fuzz/fuzz -timeout=$LIBFUZZER_TIMEOUT $LIBFUZZER_WORK_CORPUS $SEED_CORPUS" \
+  ./bitcoin/build/src/test/fuzz/fuzz \
   --time_per_mutant $TIME_PER_MUTANT \
+  --post_mutant_cmd "rm -rf $LIBFUZZER_WORK_CORPUS/ && mkdir --parents $LIBFUZZER_WORK_CORPUS" \
   --reachability_check_cmd "./bitcoin/build/src/test/fuzz/fuzz -runs=1 $REACHABILITY_CORPUS" \
   --score --avoid_repeats --verbose \
   --budget $EVAL_BUDGET \
